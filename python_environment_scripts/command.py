391d43c5fced7236077aab33d0b65383bdef9c24
scottchenghong <553218391@qq.com>
2014-01-27 15:20:46 +0800

    1

---
+++ python_environment_scripts/command.py
@@ -0,0 +1,28 @@
+#coding:utf8
+
+'''
+Created on 2014-1-26
+
+@author: chenghong
+'''
+import os
+
+def _excute_command(command):
+    '''
+    find file and setup it
+    '''
+    return os.system(command)
+
+def kill_process(port):
+    '''
+    kill process from port
+    @param port: port of process
+    @return: none 
+    '''
+    #kill porcess from port
+    command = " kill -9 $(lsof -i:7272|tail -1|awk '\"$1\"!=\"\"{print $2}')"%(port)
+    
+    _excute_command(command)
+
+if __name__ == '__main__':
+    kill_process('7272')
